7d8f7495e79b11225f00ff59156125af8d1f3d04
Nicholas Barrow <nbarrow@inspiretmstech.com>
2026-04-25 23:56:59 -0400

    chore: dashboard

--- apps/web/app/organizations/[organization-id]/page.tsx
+++ apps/web/app/organizations/[organization-id]/page.tsx
@@ -1,13 +1,30 @@
 import { H1 } from "@packages/ui/components/typography";
+import { createServerClient } from "@apps/web/lib/supabase/server";
+import { ErrorPage } from "@apps/web/components/page";
 
 export default async function Page(props: {
   params: Promise<{
     "organization-id": string;
   }>;
 }) {
+
+  const params = await props.params;
+
+  const supabase = await createServerClient();
+  const { data: { session }, error } = await supabase.auth.getSession();
+
+  if(error || !session) return (
+    <ErrorPage />
+  )
+
+  const user = await supabase.from("profiles").select().eq("user_id", session.user.id).eq("organization_id", params["organization-id"]).single();
+  if(user.error) return (
+    <ErrorPage />
+  )
+
   return (
     <div className={"flex w-full flex-col p-16"}>
-      <H1>{"Welcome back, "}</H1>
+      <H1>{`Welcome back, ${user.data.first_name}!`}</H1>
     </div>
   );
 }
