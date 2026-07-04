7d921c2e99ad51349462a4a667c4c7a448b27797
Nicholas Barrow <nbarrow@inspiretmstech.com>
2026-07-03 20:12:32 -0400

    chore: menu

--- apps/desktop/main.go
+++ apps/desktop/main.go
@@ -7,6 +7,8 @@ import (
 	"time"
 
 	"github.com/wailsapp/wails/v3/pkg/application"
+	"github.com/wailsapp/wails/v3/pkg/events"
+	"github.com/wailsapp/wails/v3/pkg/services/dock"
 )
 
 // Wails uses Go's `embed` package to embed the frontend files into the binary.
@@ -16,6 +18,7 @@ import (
 
 //go:embed all:frontend/dist
 var assets embed.FS
+var isAutoStartEnabled bool
 
 func init() {
 	// Register a custom event whose associated data type is string.
@@ -29,43 +32,90 @@ func init() {
 // logs any error that might occur.
 func main() {
 
-	// Create a new Wails application by providing the necessary options.
-	// Variables 'Name' and 'Description' are for application metadata.
-	// 'Assets' configures the asset server with the 'FS' variable pointing to the frontend files.
-	// 'Bind' is a list of Go struct instances. The frontend has access to the methods of these instances.
-	// 'Mac' options tailor the application when running an macOS.
+	dockService := dock.New()
+
 	app := application.New(application.Options{
-		Name:        "desktop",
-		Description: "A demo of using raw HTML & CSS",
+		Name:        "ProjDocs",
+		Description: "ProjDocs Desktop Client",
 		Services: []application.Service{
 			application.NewService(&GreetService{}),
+			application.NewService(dockService),
 		},
-		Assets: application.AssetOptions{
-			Handler: application.AssetFileServerFS(assets),
-		},
+		Assets: application.AssetOptions{Handler: application.AssetFileServerFS(assets)},
 		Mac: application.MacOptions{
-			ApplicationShouldTerminateAfterLastWindowClosed: true,
+			ApplicationShouldTerminateAfterLastWindowClosed: false,
 		},
 	})
 
-	// Create a new window with the necessary options.
-	// 'Title' is the title of the window.
-	// 'Mac' options tailor the window when running on macOS.
-	// 'BackgroundColour' is the background colour of the window.
-	// 'URL' is the URL that will be loaded into the webview.
-	app.Window.NewWithOptions(application.WebviewWindowOptions{
-		Title: "Window 1",
-		// Window sized to the golden ratio (1000 / 618 ≈ 1.618).
-		Width:  1000,
-		Height: 618,
-		Mac: application.MacWindow{
-			InvisibleTitleBarHeight: 50,
-			Backdrop:                application.MacBackdropTranslucent,
-			TitleBar:                application.MacTitleBarHiddenInset,
-		},
-		BackgroundColour: application.NewRGB(6, 7, 15),
-		URL:              "/",
+	if _isAutoStartEnabled, err := app.Autostart.IsEnabled(); err != nil {
+		log.Printf("Error checking if autostart is enabled: %v", err)
+		isAutoStartEnabled = false
+	} else {
+		isAutoStartEnabled = _isAutoStartEnabled
+	}
+
+	app.Event.OnApplicationEvent(events.Common.ApplicationStarted, func(event *application.ApplicationEvent) {
+		dockService.HideAppIcon()
 	})
+
+	window := app.Window.
+		NewWithOptions(application.WebviewWindowOptions{
+			Title: "ProjDocs Desktop",
+			// Window sized to the golden ratio (1000 / 618 ≈ 1.618).
+			Width:  1000,
+			Height: 618,
+			Mac: application.MacWindow{
+				InvisibleTitleBarHeight: 50,
+				Backdrop:                application.MacBackdropTranslucent,
+				TitleBar:                application.MacTitleBarHiddenInset,
+			},
+			BackgroundColour: application.NewRGB(6, 7, 15),
+			URL:              "/",
+			AlwaysOnTop:      true,
+			Hidden:           true,
+		})
+
+	window.RegisterHook(events.Common.WindowClosing, func(event *application.WindowEvent) {
+		event.Cancel()
+		window.Hide()
+	})
+
+	// Create system tray
+	systray := app.SystemTray.New()
+
+	//systray.SetIcon(icon)
+	systray.SetLabel("PD")
+	systray.
+		AttachWindow(window).
+		WindowOffset(10).
+		WindowDebounce(200 * time.Millisecond).
+		OnClick(func() {
+			if window.IsVisible() {
+				window.Hide()
+			} else {
+				window.Show()
+			}
+		})
+
+	// Set menu
+	menu := app.NewMenu()
+	menu.AddCheckbox("Start at Login", isAutoStartEnabled).OnClick(func(ctx *application.Context) {
+		isAutoStartEnabled = ctx.ClickedMenuItem().Checked()
+		if isAutoStartEnabled {
+			if err := app.Autostart.Enable(); err != nil {
+				log.Printf("Error enabling autostart: %v", err)
+			}
+		} else {
+			if err := app.Autostart.Disable(); err != nil {
+				log.Printf("Error disabling autostart: %v", err)
+			}
+		}
+	})
+	menu.AddSeparator()
+	menu.Add("Quit").OnClick(func(ctx *application.Context) {
+		app.Quit()
+	})
+	systray.SetMenu(menu)
 
 	// Create a goroutine that emits an event containing the current time every second.
 	// The frontend can listen to this event and update the UI accordingly.
